3c1c16491327ae2196659f3960dd738dd8a19f01
AkhmadFaqihh <akhmad.faqih.ft20@mail.umy.ac.id>
2023-04-01 00:02:22 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$AXT0rtSDq9FcVp.4CgcgHeKnQYZSMfECpA.qU8036AF9ElHl8IGuO', // password
+                'remember_token'=> Str::random(10),
+                'is_admin' => true
+            ]
+        );
+        User::factory(100)->create();
+        Todo::factory(500)->create();
     }
 }
