aadca76090dfbab922a5f08c6a7af4b564254bea
AkhmadFaqihh <akhmad.faqih.ft20@mail.umy.ac.id>
2023-04-13 01:33:35 +0700

    create, read, search data

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,10 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
 Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
 Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
 
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
+
